817d552c5e844980937dd9c144b6102c7dd01d07
ADelooze <alex.delooze@gmail.com>
2016-05-13 22:20:25 +0100

    Timeline slider
    Plugin from https://github.com/IonDen/ion.rangeSlider

--- WebAppOddsMatcherUtility/Scripts/_references.js
+++ WebAppOddsMatcherUtility/Scripts/_references.js
@@ -1,6 +1,7 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="ai.0.15.0-build58334.js" />
 /// <reference path="bootstrap.js" />
+/// <reference path="ion.rangeslider.min.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="jquery-1.10.2.js" />
